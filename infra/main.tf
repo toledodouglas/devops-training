3417e68a8d6841e0c0a8d57c0be6c2880d4882df
douglas <douglas.rtoledo2003@gmail.com>
2026-04-16 17:27:56 -0300

    feat: created infrastructure base in terraform

---
+++ infra/main.tf
@@ -0,0 +1,37 @@
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+
+  tags = {
+    projeto    = "devops-training"
+    ambiente   = var.environment
+    managed_by = "terraform"
+  }
+}
+
+# Service Plan - Infraestrutura de computação (máquina virtual - Hardware)
+resource "azurerm_service_plan" "plan" {
+  name                = "sp-devops-training-${var.environment}"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  os_type             = "Linux"
+  sku_name            = var.app_service_sku
+}
+
+resource "azurerm_linux_web_app" "app" {
+  name                = "app-devops-training-${var.environment}-toledo" 
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_service_plan.plan.location
+  service_plan_id     = azurerm_service_plan.plan.id
+
+  site_config {
+    application_stack {
+      dotnet_version = "8.0" 
+    }
+    always_on = false 
+  }
+
+  app_settings = {
+    "ASPNETCORE_ENVIRONMENT" = var.environment == "prod" ? "Production" : "Development"
+  }
+}
